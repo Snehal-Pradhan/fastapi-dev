f1360f93d329911fa10d655c98b1d21e7d10751e
Snehal Pradhan <snehalpradhan23@gmail.com>
2025-10-18 06:15:13 +0530

    added  main.py

--- 01_fastapi_crud/main.py
+++ 01_fastapi_crud/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class User(BaseModel):
+    id : int
+    name : str
+    email : str
+    is_active : bool = True
+
 @app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
+def home():
+    return {"message" : "home route"}
+
+@app.get("/users/{user_id}")
+def user_by_user_id(user_id:int):
+    return {"user" : "present","user_id" : user_id}
+
+@app.get("/search")
+def search_by_name(user_name: str):
+    return {"user" : user_name}
